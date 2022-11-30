2e4e34e0e79342c3f05ed8d77aefa5376b203ce6
Francis Bui <francis-bui@outlook.com>
2022-11-29 21:50:45 -0500

    breakout exception added

--- main.py
+++ main.py
@@ -6,30 +6,36 @@ import os
 import time
 import RPi.GPIO as GPIO
 
+class CodeFinished(Exception): pass
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def button_callback(channel):
+try:
+    while True:
 
-    camera = PiCamera()
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    camera.capture('image/' + str(timestr))
+        def button_callback(channel):
 
-    #Open the test image
-    image = Image.open('image/' + str(timestr))
+            camera = PiCamera()
+            timestr = time.strftime("%Y%m%d-%H%M%S")
+            camera.capture('image/' + str(timestr))
 
-    #Convert the image to grayscale
-    gray = ImageOps.grayscale(image)
+            image = Image.open('image/' + str(timestr))
+            gray = ImageOps.grayscale(image)
 
-    #Temporarily save the grayscale file
-    filename = f"{timestr}.png".format(os.getpid())
-    gray.save(filename)
+            #Temporarily save the grayscale file
+            filename = f"{timestr}.png".format(os.getpid())
+            gray.save(filename)
 
-    #Open the grayscale file and run OCR on it
-    text = pytesseract.image_to_string(Image.open(filename))
+            #Open the grayscale file and run OCR on it
+            text = pytesseract.image_to_string(Image.open(filename))
+            print(text)
 
-    print(text)
-    
-GPIO.add_event_detect(5,GPIO.RISING,callback=button_callback)
-GPIO.cleanup()
+            raise CodeFinished
+
+        GPIO.add_event_detect(5,GPIO.RISING,callback=button_callback)
+
+except CodeFinished:
+
+    GPIO.cleanup()
